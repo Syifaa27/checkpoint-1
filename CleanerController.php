e88f35c746dc6151ffe6cb9b7d9b85baa8c4e55f
Syifaa27 <43197833+Syifaa27@users.noreply.github.com>
2019-07-31 09:50:09 +0700

    Add files via upload

---
+++ CleanerController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CleanerController extends Controller
+{
+    //
+    public function index()
+    {
+        //
+       //  echo "saya kelihatan";
+
+       return view('table');
+    }
+}
